930afd449e5210219f66db8cd335fb3135b9e2d0
Rob u Hoodici <RobUHoodici@proton.me>
2024-09-08 14:47:32 +0200

    feat: :rocket: introduce initial codebase for website hosted via GitHub pages

---
+++ src/App.jsx
@@ -0,0 +1,104 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ fontFamily: "Arial, sans-serif", margin: "20px" }}>
+      <header
+        style={{
+          borderBottom: "1px solid #ccc",
+          paddingBottom: "10px",
+          marginBottom: "20px",
+        }}
+      >
+        <h1>Rob u Hoodici</h1>
+      </header>
+      <main>
+        <article style={{ marginBottom: "20px", textAlign: "left" }}>
+          <p>
+            <b>Operacija "Robin Hood - Zaustavimo poreznog Šerifa"</b>
+          </p>
+          <p>
+            Ponukani velikim nezadovoljstvom građana u vezi{" "}
+            <a
+              href="https://www.porezna-uprava.hr/Stranice/Pravila-nagradnog-natjecanja-Bez-racuna-se-ne-racuna.aspx"
+              _target="blank"
+            >
+              nagradne <strike>igre</strike> natječaja
+            </a>{" "}
+            porezne uprave. O kojoj možete pročitati više u{" "}
+            <a
+              href="https://www.index.hr/vijesti/clanak/nagradna-igra-bez-racuna-se-ne-racuna-ima-niz-problema-porezna-suti/2596296.aspx"
+              _target="blank"
+            >
+              novinskim člancima
+            </a>{" "}
+            i{" "}
+            <a
+              href="https://www.reddit.com/r/croatia/comments/1f9gu59/nagradna_igra_porezne_uprave/?rdt=38900"
+              _target="blank"
+            >
+              Reddit-u
+            </a>
+            .
+          </p>
+          <p>
+            Odlučili smo se na akciju koja će pokazati da porezna uprava ne može
+            raditi što god želi. I davati nagradu svojim prijateljima i
+            poznanicima, mjenjajući pravila igre. Naime, porezna uprava je
+            promijenila pravila igre usred nagradnog natječaja, a da nije
+            obavijestila igrače. Premda imaju mogućnosti slanja push
+            notifikacija unutar aplikacije kao i sustav registracije za nagradni
+            natječaj te prihvata pravila natjecanja da bi uopće mogli
+            sudjelovati. Za to su imali nevjerojatnih HRK 18.820.000,00{" "}
+            <a href="https://www.porezna-uprava.hr/mPorezna/Stranice/O_projektu_mPorezna.aspx">
+              više detalja o projektu
+            </a>
+          </p>
+          <p>
+            Stoga predlažemo ideju pod nazivom operacija "Robin Hood -
+            Zaustavimo poreznog Šerifa", kako porezni Šerif ne bi dao nagradu
+            svojim prijateljima i podobnima bolje da tu nagradu indirektno bude
+            primoran dati u dobrotvorne svrhe. Neka Index prijavi 3 svoja
+            novinara, dobrovoljca, za svaki mjesec kola, koji bi javno obećali
+            da će dati 100% nagrade u dobrotvorne svrhe te ćemo Vam
+            mi/čitatelji/hredditovci/btardi/degeni/ljudi-dobre-volje poslati
+            svaki dan i preko 300 validnih fiskaliziranih računa. Također
+            zamolili bi Index da otvori mail adresu gdje će vam svi moći slati
+            račune koji žele pomoći djeci ili nekoj dobrotvornoj organizaciji.
+          </p>
+          <p>
+            <b>
+              Svi su dobrodošli da se pridruže u borbi protiv poreznog Šerifa.
+            </b>
+          </p>
+          <p>
+            U ovoj borbi ključna je pomoć novinara, Index je zaprimio mail o
+            predloženoj ideji 08.09.2024. čekamo odgovor kako bi krenuli u
+            borbu. Naime porezna uprava može blokirati privatne osobe koje imaju
+            više bodova od Stevena, Zvonka i Alena, kao što imamo informaciju da
+            su to već učinili, ali puno teže može blokirati novinare.
+          </p>
+          <p>
+            Ukoliko se Index ne odazove našem pozivu, pozivamo sve građane
+            dobrovoljce da potpišu izjavu kako će dati nagradu u dobrotvorne
+            svrhe. (Može putem Certilie) A naš team [object Object] će se
+            potruditi da dobijete dnevno veliki broj računa na vašu email
+            adresu.
+          </p>
+        </article>
+      </main>
+      <footer
+        style={{
+          borderTop: "1px solid #ccc",
+          paddingTop: "10px",
+          marginTop: "20px",
+        }}
+      >
+        <p>Neka znanje nadvlada zlo i nesposobnost!</p>
+        <p>Update uskoro!</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
